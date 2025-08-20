18e73e81a9b3172f6fd87fcd682f9160c583f84f
swabah <ahmedswabah922@gmail.com>
2025-08-20 17:42:12 +0530

    update backend

--- backend/src/middleware/error.js
+++ backend/src/middleware/error.js
@@ -17,6 +17,30 @@ const errorConverter = (err, req, res, next) => {
   next(error);
 };
 
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, req, res, next) => {
+  let { statusCode, message } = err;
+  if (config.env === "production" && !err.isOperational) {
+    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
+  }
+
+  res.locals.errorMessage = err.message;
+
+  const response = {
+    code: statusCode,
+    message,
+    ...(config.env === "development" && { stack: err.stack }),
+  };
+
+  if (config.env === "development") {
+    logger.error(err);
+  }
+
+  res.status(statusCode).send(response);
+};
+
 module.exports = {
   errorConverter,
+  errorHandler,
 };
