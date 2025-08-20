a624cb47aedd65953d465bf99e408697a3f093b6
swabah <ahmedswabah922@gmail.com>
2025-08-20 15:41:00 +0530

    updated server,js

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,39 +2,24 @@ require("dotenv").config();
 const app = require("./app");
 const mongoose = require("mongoose");
 
-let server;
-const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
-// DB Connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
+let isConnected = false;
+
+const connectToDb = async () => {
+  if (!isConnected) {
+    await mongoose.connect(MONGO_URI);
+    isConnected = true;
     console.log("Connected to MongoDB");
-  })
-  .catch((err) => console.error("MongoDB connection error", err));
-
-const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      console.log("Server closed");
-      process.exit(1);
-    });
-  } else {
-    process.exit(1);
   }
 };
 
-const unexpectedErrorHandler = (error) => {
-  console.log(error);
-  exitHandler();
-};
-
-process.on("uncaughtException", unexpectedErrorHandler);
-process.on("unhandledRejection", unexpectedErrorHandler);
-
-process.on("SIGTERM", () => {
-  console.log("SIGTERM received");
-  if (server) {
-    server.close();
+module.exports.handler = serverless(async (req, res) => {
+  try {
+    await connectToDb();
+    app(req, res);
+  } catch (error) {
+    console.error("Error in handler:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
