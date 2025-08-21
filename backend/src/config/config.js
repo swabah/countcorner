b3ccc7e464003588c49922f80107e23087a05145
swabah <ahmedswabah922@gmail.com>
2025-08-21 22:01:13 +0530

    modified

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -8,7 +8,6 @@ const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string()
       .valid("production", "development", "test")
-      .default("development")
       .required(),
     PORT: Joi.number().default(5000),
     MONGODB_URL: Joi.string().required().description("Mongo DB url"),
@@ -18,10 +17,6 @@ const envVarsSchema = Joi.object()
 const { value: envVars, error } = envVarsSchema
   .prefs({ errors: { label: "key" } })
   .validate(process.env);
-
-if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
-}
 
 module.exports = {
   env: envVars.NODE_ENV,
